811f1cd31432a823b2060c687720c01627e53be5
Harsh <harshc3294@gmail.com>
2020-10-05 13:56:18 +0530

    initial commit

---
+++ plan.md
@@ -0,0 +1,13 @@
+
+1. Create Wireframe=top level of design
+2. Create react app
+3. cleanup project
+4. Structure of COVID Tracker
+5. Create header
+6. Create info boxes
+7. Create Tables
+8. Create Chart
+9. Create Map
+10. styling
+11. Material UI
+12. Deploy
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASCfO2wkNBTJkMkyfppHhUG35s_ZZdcm4",
+    authDomain: "covid-19-tracker-22974.firebaseapp.com",
+    databaseURL: "https://covid-19-tracker-22974.firebaseio.com",
+    projectId: "covid-19-tracker-22974",
+    storageBucket: "covid-19-tracker-22974.appspot.com",
+    messagingSenderId: "561896895786",
+    appId: "1:561896895786:web:f81f72d09525abd2dd5a53",
+    measurementId: "G-HC8492HZKN"
+  };
